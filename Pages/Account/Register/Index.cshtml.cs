c297cf6004054f5abd3dafa8b6384a1638e91dcf
Дмитрий Загарий <dzagariy@gmail.com>
2023-11-10 10:10:03 +0300

    Описание изменений f

---
+++ Pages/Account/Register/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServerAspNetIdentity.Pages.Account.Register
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
